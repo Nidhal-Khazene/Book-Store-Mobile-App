7ad3aff27370b4976c211fa43e43911bcec22963
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-24 13:52:39 +0100

    add book details view and best seller listview

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,8 +1,8 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_listview_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_listview.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
+import 'custom_app_bar.dart';
 import 'featured_listview_builder.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -12,15 +12,21 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedListView(),
-          const SizedBox(height: 52),
-          Text("Best Seller", style: Styles.textStyle18),
-          const SizedBox(height: 21),
-          BestSellerListViewItem(),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomAppBar(),
+                FeaturedListView(),
+                const SizedBox(height: 52),
+                Text("Best Seller", style: Styles.textStyle18),
+                const SizedBox(height: 21),
+              ],
+            ),
+          ),
+          const SliverFillRemaining(child: BestSellerListView()),
         ],
       ),
     );
