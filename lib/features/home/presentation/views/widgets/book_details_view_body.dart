10e86de250059dd0f5e1ec4fa4b2964c0cf87e0c
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-25 16:24:52 +0100

    add title and image to book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar_book_details.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -7,9 +10,32 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: kPadding30),
-      child: SafeArea(child: Column(children: [CustomAppBarBookDetails()])),
+      child: SafeArea(
+        child: Column(
+          children: [
+            CustomAppBarBookDetails(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .2),
+              child: CustomBookImage(),
+            ),
+            const SizedBox(height: 45),
+            Text("The Jungle Book", style: Styles.textStyle30),
+            const SizedBox(height: 9),
+            Opacity(
+              opacity: 0.7,
+              child: Text(
+                "Rudyard Kipling",
+                style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic),
+              ),
+            ),
+            const SizedBox(height: 12),
+            BookRating(),
+          ],
+        ),
+      ),
     );
   }
 }
