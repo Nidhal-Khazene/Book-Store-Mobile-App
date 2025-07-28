1af0db06e0974af44014c54f1e6114b890c1391e
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-28 18:47:33 +0100

    implement home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,7 +3,7 @@ import 'package:gramaz_app/core/errors/failures.dart';
 import 'package:gramaz_app/features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
 
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:dartz/dartz.dart';
 import 'package:gramaz_app/core/errors/failures.dart';
+import 'package:gramaz_app/core/utils/api_service.dart';
 import 'package:gramaz_app/features/home/data/models/book_model/book_model.dart';
 import 'package:gramaz_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImplementation extends HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImplementation({required this.apiService});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            "volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest",
+      );
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServiceFailure());
+    }
   }
 
   @override
