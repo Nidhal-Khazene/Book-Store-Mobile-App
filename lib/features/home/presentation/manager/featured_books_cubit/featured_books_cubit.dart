f8e0f7b09d5ae69a4d127271d21ce471ab8504ca
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-30 20:44:18 +0100

    implement featured books cubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:gramaz_app/core/errors/failures.dart';
 import 'package:gramaz_app/features/home/data/models/book_model/book_model.dart';
+import 'package:gramaz_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (Failure failure) {
+        emit(FeaturedBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(FeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
