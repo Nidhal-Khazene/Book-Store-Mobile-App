41df79c9bebfadcf3e0b9d88f2fe7b942f44ff36
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-30 20:57:58 +0100

    create newest cubit

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:gramaz_app/features/home/data/models/book_model/book_model.dart';
+import 'package:gramaz_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
+
+import '../../../../../core/errors/failures.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (Failure failure) {
+        emit(NewestBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
 }
