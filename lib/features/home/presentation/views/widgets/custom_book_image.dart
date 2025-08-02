602ff0c2946063d5050ee90b4e726894b41ac422
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-02 05:45:15 +0100

    chached network image testing and some additives

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,16 +1,22 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({super.key, required this.image});
+
   final String image;
+
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(fit: BoxFit.fill, image: NetworkImage(image)),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: image,
+          fit: BoxFit.fill,
+          errorWidget: (context, url, error) =>
+              Icon(Icons.error_outline, size: 32),
         ),
       ),
     );
