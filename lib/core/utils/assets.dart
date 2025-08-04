8b7415d9588cb16c0234240a5a354697488e8d3f
nidhal khazene <nidhal.khazene@gmail.com>
2025-08-04 19:15:11 +0100

    delete some images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,8 +1,4 @@
 class AssetsData {
   static const String mainLogo = "assets/images/main_logo.png";
   static const String splashLogo = "assets/images/splash_logo.png";
-  static const String testImage = "assets/images/test_book_image.png";
-  static const String testImage2 = "assets/images/test2.png";
-  static const String tempImage =
-      "https://onepagelove.imgix.net/2021/03/opl-master-6.jpg?max-h=4096&min-w=1280&fit=crop&fp-y=0&auto=compress";
 }
